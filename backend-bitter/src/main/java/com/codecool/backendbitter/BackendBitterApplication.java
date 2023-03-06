5714ea3478113a1e27d3a1c17bd1f7540adc14cb
Máté <mate.lorinczi97@gmail.com>
2023-03-06 15:00:16 +0100

    Init backend

---
+++ backend-bitter/src/main/java/com/codecool/backendbitter/BackendBitterApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.backendbitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBitterApplication.class, args);
+	}
+
+}
---
+++ backend-bitter/src/test/java/com/codecool/backendbitter/BackendBitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.backendbitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
