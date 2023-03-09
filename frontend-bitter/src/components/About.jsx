1a35f81417550601b6f8e4f90120aea8f77c4ead
mezeipb <mezei.peter.barnabas@gmail.com>
2023-03-09 14:30:05 +0100

    Add App class to BitFeed and change some About facts

--- frontend-bitter/src/components/About.jsx
+++ frontend-bitter/src/components/About.jsx
@@ -7,13 +7,11 @@ function Main() {
             <div className="App">
                 <h1>Fun facts about Bitter</h1>
                 <ul className="facts">
-                    <li>It is said to be better than Twitter</li>
-                    <li>Space Karen hates Bitter</li>
                     <li>Bitter is much more popular among developers</li>
                     <li>Expected to get 400million users by 2024</li>
-                    <li>
-                        Looks better
-                    </li>
+                    <li>Looks awesome</li>
+                    <li>Bitter will change the world</li>
+                    <li>"Never let bitterness make a home in your heart; raise the rent and kick it out"</li>
                 </ul>
             </div>
         </>
