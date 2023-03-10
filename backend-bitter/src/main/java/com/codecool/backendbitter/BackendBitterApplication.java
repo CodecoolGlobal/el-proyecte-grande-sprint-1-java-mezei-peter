7273fae8a40844dfda7673a5b070b45d35222e07
Bence Fabian <bence.fabian@codecool.com>
2023-03-10 11:23:49 +0100

    example code for initilization

--- backend-bitter/src/main/java/com/codecool/backendbitter/BackendBitterApplication.java
+++ backend-bitter/src/main/java/com/codecool/backendbitter/BackendBitterApplication.java
@@ -1,14 +1,20 @@
 package com.codecool.backendbitter;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class BackendBitterApplication {
+public class BackendBitterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendBitterApplication.class, args);
 	}
 
+        @Override
+        public void run(String... args) throws Exception {
+            // possible initialization code here (if it is done via beans
+            // then it can even differ profile-by-profile)
+        }
 }
