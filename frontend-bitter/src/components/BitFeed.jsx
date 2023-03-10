bc94d1417f10aec3e9aa020ffde7b98b018431f8
Bence Fabian <bence.fabian@codecool.com>
2023-03-10 11:38:05 +0100

    prepare feed UI element to switching displayed user on-the-fly

--- frontend-bitter/src/components/BitFeed.jsx
+++ frontend-bitter/src/components/BitFeed.jsx
@@ -2,19 +2,33 @@ import {useState, useEffect} from "react";
 import BitCard from "./BitCard.jsx";
 
 function BitFeed() {
+    const [user, setuser] = useState('abc');
     const [feedBits, setFeedBits] = useState(null);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
-        fetch("/bit/feed/abc")
-            .then(response => response.json())
+        const abortController = new AbortController();
+        const abortFetch = () => {
+            // TODO: we might get stuck in the "loading" state here
+            // Maybe a "something went wrong" state?
+            abortController.abort()
+        };
+        setLoading(true);
+        fetch(`/bit/feed/${user}`, { signal: abortController.signal })
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json()
+                } else {
+                    // now what?
+                }
+            })
             .then(data => {
                 console.log(data)
                 setFeedBits(data)
                 setLoading(false)
             });
 
-
-    }, []);
+        return abortFetch;
+    }, [user]);
 
     if (loading) {
         return <div className="App">LOADING</div>
@@ -23,7 +37,6 @@ function BitFeed() {
         <>
             <div className="BitFeed App">
                 {feedBits.map(bit => <BitCard bit={bit} key={bit.bitId}/>)}
-
             </div>
         </>
     );
