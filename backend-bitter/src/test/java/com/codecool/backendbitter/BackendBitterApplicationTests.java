44ca00459d63c1c5756f8baca8606c58152f012b
mezeipb <mezei.peter.barnabas@gmail.com>
2023-03-20 14:05:00 +0100

    Add environment variable values to application.properties

--- backend-bitter/src/test/java/com/codecool/backendbitter/BackendBitterApplicationTests.java
+++ backend-bitter/src/test/java/com/codecool/backendbitter/BackendBitterApplicationTests.java
@@ -3,11 +3,11 @@ package com.codecool.backendbitter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class BackendBitterApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
