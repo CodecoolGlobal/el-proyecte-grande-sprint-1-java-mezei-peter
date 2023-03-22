4bed47d1718076b62e55ea3c86c75ad8eda2456a
Nemeth Andras <nthas13@gmail.com>
2023-03-22 14:56:30 +0100

    create putrequest for like

--- backend-bitter/src/main/java/com/codecool/backendbitter/controller/BitController.java
+++ backend-bitter/src/main/java/com/codecool/backendbitter/controller/BitController.java
@@ -2,11 +2,12 @@ package com.codecool.backendbitter.controller;
 
 import com.codecool.backendbitter.controller.dto.BitDTO;
 import com.codecool.backendbitter.controller.dto.GeneralUserDTO;
-import com.codecool.backendbitter.model.profilePicture.LowResProfilePicture;
+import com.codecool.backendbitter.service.BitService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.PrintStream;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,15 +18,37 @@ import java.util.UUID;
 @CrossOrigin(origins = "http://localhost:5173")
 public class BitController {
 
+    private final BitService bitService;
+
+    public BitController(BitService bitService) {
+        this.bitService = bitService;
+    }
+
     @GetMapping("/feed/{userId}")
     public ResponseEntity<List<BitDTO>> getBits(@PathVariable String userId) {
         List<BitDTO> dummyData = List.of(new BitDTO(UUID.randomUUID(),
                 new GeneralUserDTO(UUID.randomUUID(), "Dummy User 1", true, false,
-                        "Profile Picture"), Timestamp.valueOf(LocalDateTime.now()) , "Dummy Content"
-                ), new BitDTO(UUID.randomUUID(),
+                        "Profile Picture"), Timestamp.valueOf(LocalDateTime.now()), "Dummy Content"
+        ), new BitDTO(UUID.randomUUID(),
                 new GeneralUserDTO(UUID.randomUUID(), "Dummy User 2", true, false,
-                        "Profile Picture"), Timestamp.valueOf(LocalDateTime.now()) , "Dummy Content"
+                        "Profile Picture"), Timestamp.valueOf(LocalDateTime.now()), "Dummy Content"
         ));
         return new ResponseEntity<>(dummyData, HttpStatus.OK);
+    }
+
+
+    @PutMapping("/{bitId}/like/{userId}")
+    public ResponseEntity<Void> likeBits(@PathVariable String bitId, @PathVariable String userId) {
+
+        try {
+            UUID finalUserId = UUID.fromString(userId);
+            UUID finalBitId = UUID.fromString(bitId);
+
+            bitService.likeBit(finalUserId, finalBitId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Throwable e) {
+            e.printStackTrace(System.err);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
