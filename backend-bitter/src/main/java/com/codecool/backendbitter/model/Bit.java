0bf9891d1b5bf0cb2b8387bab4a25615f360d19e
FZF-Dev <fzoltan123456@gmail.com>
2023-03-23 09:21:44 +0100

    Add {@AllArgsConstructor, @Builder} to Bit

--- backend-bitter/src/main/java/com/codecool/backendbitter/model/Bit.java
+++ backend-bitter/src/main/java/com/codecool/backendbitter/model/Bit.java
@@ -2,6 +2,8 @@ package com.codecool.backendbitter.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +15,8 @@ import java.util.UUID;
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table
 public class Bit {
     @Id
