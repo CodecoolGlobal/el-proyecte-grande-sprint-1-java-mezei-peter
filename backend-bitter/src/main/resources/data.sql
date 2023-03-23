7615a378b32ecbfc5988ba539dc86f1115269f70
FZF-Dev <fzoltan123456@gmail.com>
2023-03-23 15:19:02 +0100

    Delete first line in data.sql

--- backend-bitter/src/main/resources/data.sql
+++ backend-bitter/src/main/resources/data.sql
@@ -1,4 +1,3 @@
---Insert Into users VALUES ('026fa192-4de8-4b9e-902a-f9d029961302', current_timestamp, false, false, 'admin', 'test_picture', 'test bio', 'test_email', 'test_user');
 Insert Into bit VALUES ('026fa192-4de8-4b9e-902a-f9d029961302', 'Test Content', current_timestamp, '00000000-0000-0000-0000-000000000000');
 Insert INTO users_bits VALUES ('00000000-0000-0000-0000-000000000000', '026fa192-4de8-4b9e-902a-f9d029961302');
 
