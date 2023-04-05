44fc39e505a401ac94fa71e09bf168b60386da3e
mezeipb <mezei.peter.barnabas@gmail.com>
2023-04-05 13:52:03 +0200

    Place Authentication token into SecurityContext

--- backend-bitter/src/main/java/com/codecool/backendbitter/security/JwtAuthenticationFilter.java
+++ backend-bitter/src/main/java/com/codecool/backendbitter/security/JwtAuthenticationFilter.java
@@ -1,14 +1,18 @@
 package com.codecool.backendbitter.security;
 
 import com.codecool.backendbitter.service.jwt.JwtService;
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.naming.AuthenticationException;
@@ -16,9 +20,11 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService) {
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -33,8 +39,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final int BEARER_PREFIX_LENGTH = 7;
         String token = authorization.substring(BEARER_PREFIX_LENGTH);
         try {
-            jwtService.readTokenBodySubject(token);
-        } catch (AuthenticationException e) {
+            String userSubject = jwtService.readTokenBodySubject(token);
+            UserDetails user = userDetailsService.loadUserByUsername(userSubject);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    user.getUsername(), user.getPassword()
+            );
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+        } catch (AuthenticationException | UsernameNotFoundException e) {
             System.err.println(e.getMessage());
         }
 
--- backend-bitter/src/main/java/com/codecool/backendbitter/security/SecurityConfig.java
+++ backend-bitter/src/main/java/com/codecool/backendbitter/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,7 +32,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtService jwtService,
-                                                   AuthenticationManager authenticationManager) throws Exception {
+                                                   AuthenticationManager authenticationManager,
+                                                   UserDetailsService userDetailsService) throws Exception {
         http.csrf().disable();
         http.authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/user/login", "/user/registration").permitAll()
@@ -39,7 +41,7 @@ public class SecurityConfig {
                 .and()
                 .cors();
         http.addFilter(new CustomUsernamePasswordAuthenticationFilter(jwtService, authenticationManager));
-        http.addFilterBefore(new JwtAuthenticationFilter(jwtService), CustomUsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtService, userDetailsService), CustomUsernamePasswordAuthenticationFilter.class);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
