fd69b2539ecb6e2f3530e5b5caa04525c0a1078d
mezeipb <mezei.peter.barnabas@gmail.com>
2023-04-05 10:51:29 +0200

    Test generate token in JjwtJwtService

--- backend-bitter/src/test/java/com/codecool/backendbitter/service/jwt/JjwtJwtServiceTest.java
+++ backend-bitter/src/test/java/com/codecool/backendbitter/service/jwt/JjwtJwtServiceTest.java
@@ -9,16 +9,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JjwtJwtServiceTest {
     @Test
+    void testGenerateToken() {
+        JjwtJwtService jjwtJwtService = new JjwtJwtService(Decoders.BASE64.decode(
+                "testkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkey"));
+        jjwtJwtService.generateToken("test");
+    }
+
+    @Test
     void testReadToken_ValidSecretKey() throws AuthenticationException {
-        JjwtJwtService jjwtJwtService = new JjwtJwtService(Decoders.BASE64.decode("testkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkey"));
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEifQ.0xX_ccX9BzXIYcBZR71MSGSoqffWheSChwLtt33pudE";
+        JjwtJwtService jjwtJwtService = new JjwtJwtService(Decoders.BASE64.decode(
+                "testkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkey"));
+
+        String token =
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEifQ.0xX_ccX9BzXIYcBZR71MSGSoqffWheSChwLtt33pudE";
+
         jjwtJwtService.readToken(token);
     }
 
     @Test
     void testReadToken_InvalidKeyThrowsError() {
-        JjwtJwtService jjwtJwtService = new JjwtJwtService(Decoders.BASE64.decode("testkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkey"));
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEifQ.invalid9BzXIYcBZR71MSGSoqffWheSChwLtinvalid";
+        JjwtJwtService jjwtJwtService = new JjwtJwtService(Decoders.BASE64.decode(
+                "testkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkeytestkey"));
+
+        String token =
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEifQ.invalid9BzXIYcBZR71MSGSoqffWheSChwLtinvalid";
+
         assertThrows(AuthenticationException.class, () -> jjwtJwtService.readToken(token));
     }
 }
