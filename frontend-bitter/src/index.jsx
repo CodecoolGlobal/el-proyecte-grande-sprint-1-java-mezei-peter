601c12c3b7a4c753629e952b3ec377a0b5decf47
Máté <mate.lorinczi97@gmail.com>
2023-04-05 17:51:37 +0200

    Sasfety commit

--- frontend-bitter/src/index.jsx
+++ frontend-bitter/src/index.jsx
@@ -6,6 +6,7 @@ import Layout from "./components/Layout";
 import ErrorPage from "./components/ErrorPage";
 import Login from "./components/Login";
 import BitFeed from "./components/BitFeed.jsx";
+import Register from "./components/Register.jsx";
 import { UserContextProvider } from "./contexts/UserContext";
 
 const router = createBrowserRouter([
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
                 path: "/login",
                 element: <Login />,
             },
+            {
+                path: "/register",
+                element: <Register />
+            }
 
         ],
     },
