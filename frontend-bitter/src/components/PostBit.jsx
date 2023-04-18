8ddcfcbb130d2834dbd4a70a720416afcc8e1ecb
mezeipb <mezei.peter.barnabas@gmail.com>
2023-04-18 11:57:46 +0200

    Justify content in PostBit

--- frontend-bitter/src/components/PostBit.jsx
+++ frontend-bitter/src/components/PostBit.jsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 function PostBit() {
     return (
         <div className="bg-[#FFFBE9] w-full pt-6">
-            <div className="w-4/5 m-auto flex flex-col p-10 border-x-4 border-black">
+            <div className="w-4/5 m-auto flex flex-col justify-between p-10 border-x-4 border-black">
                 <textarea className="resize-none h-32 p-2" placeholder="What's on your mind?"></textarea>
                 <Button
                     sx={{
