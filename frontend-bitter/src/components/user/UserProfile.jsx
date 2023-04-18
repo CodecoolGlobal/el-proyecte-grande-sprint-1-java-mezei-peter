4b44678f8708395e76d771d0e0862ea4144baed4
FZF-Dev <fzoltan123456@gmail.com>
2023-04-18 15:01:26 +0200

    save changes to Modal.jsx

--- frontend-bitter/src/components/user/UserProfile.jsx
+++ frontend-bitter/src/components/user/UserProfile.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Modal from "../Modal.jsx";
 
 function UserProfile(props) {
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
       <div className="className=sm:p-8 px-4 py-8 w-full bg-[#FFFBE9] min-h-[calc(100vh-73px)]">
           <h1>user profile here</h1>
+          <Modal content={"content"} isOpen={true} setIsOpen={setIsOpen} />
       </div>
     );
 }
