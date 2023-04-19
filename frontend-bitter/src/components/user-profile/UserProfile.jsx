8319f6741e66f3b9fbb924c3bd7a8855c27824ad
FZF-Dev <fzoltan123456@gmail.com>
2023-04-19 11:31:57 +0200

    add useFetch custom hook

--- frontend-bitter/src/components/user-profile/UserProfile.jsx
+++ frontend-bitter/src/components/user-profile/UserProfile.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Modal from "../Modal/Modal.jsx";
+import useFetch from "../../hooks/useFetch.js";
 
 function UserProfile(props) {
     const [followersIsOpen, setFollowersIsOpen] = useState(false);
     const [followedIsOpen, setFollowedIsOpen] = useState(false);
+    const fetched = useFetch("https://swapi.dev/api/people/1/");
 
     return (
       <div className="className=sm:p-8 px-4 py-8 w-full bg-[#FFFBE9] min-h-[calc(100vh-73px)]">
